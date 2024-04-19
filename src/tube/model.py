35ed83ec2b482591a31304738127776d96079d00
a.e.kulishkin <zarelly2013@yandex.ru>
2024-04-19 16:04:34 +0500

    testing

--- src/tube/model.py
+++ src/tube/model.py
@@ -1,26 +1,13 @@
-from tensorflow.keras.models import load_model
 import joblib
+import zipfile
 
-modelsPath = 'src/tube/assets/models'
-scalersPath = 'src/tube/assets/scalers'
+class DTreeForIMA:
+    def __init__(self, filename):
+        self.filename = filename
+        with zipfile.ZipFile(f'{self.filename}.zip', 'r') as zip_ref:
+            zip_ref.extractall()
+        self.model = joblib.load(f'tube/{self.filename}.pkl')
 
-
-class SSiTModel:
-    def __init__(self, pipeChars):
-        self.model = self.load_model(pipe=pipeChars)
-        self.scaler = self.load_scaler(pipe=pipeChars)
-
-    def load_model(self, pipe):
-        return load_model('/'.join([modelsPath, f'Pipe_with_params_{pipe}_model.h5']))
-
-    def load_scaler(self, pipe):
-        return joblib.load('/'.join([scalersPath,f'std_scaler_{pipe}.bin']))
-
-    def changePipe(self, pipeChars):
-        self.model = self.load_model(pipe=pipeChars)
-        self.scaler = self.load_scaler(pipe=pipeChars)
-
-    def predict(self, data):
-        scaled_data = self.scaler.transform(data)
-        preds = self.model.predict(scaled_data).astype(float)
-        return preds
+    def predict(self, X_test):
+        pred_dtr = self.model.predict(X_test)
+        return pred_dtr
