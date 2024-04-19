9c5c74dff9243a275f9e132da65736a4bca7e9f2
Chelovek <167432039+aswsawswa@users.noreply.github.com>
2024-04-19 16:28:31 +0500

    Update model.py

--- src/well/model.py
+++ src/well/model.py
@@ -1,20 +1,45 @@
 import pandas as pd
 import numpy as np
 import pickle
+import shutil
+import os
+
+
 class Well_solver:
-    def __init__(self,name,papka_with_models='wellmodels/'):
+    def __init__(self,name,papka_with_models='wellmodels/',archive_path='wellmodels.zip'):
         self.papka_with_models = papka_with_models
         self.name = name
+        self.archive_path=archive_path
+
+        well_name=self.name
+        path = self.papka_with_models+'wellmodel_'+well_name+'.pickle'
+        check_file = os.path.isfile(path)
+        if not check_file:
+          path=self.papka_with_models+'wellmodel_'+well_name+'.pickle'
+          shutil.unpack_archive(self.archive_path,'wellmodels', 'zip')
+
+        
+
+
         path=self.papka_with_models+'norm.pickle'
+
+        
 
         with open(path, "rb") as file:
           self.normalizer = pickle.load(file)
-        well_name=self.name
-        if self.name =='"896Б"':
-          well_name='_896Б_'
-        if self.name =='896Б':
-          well_name='_896Б_'
+        
+        # if self.name =='"896Б"':
+        #   well_name='_896Б_'
+        # if self.name =='896Б':
+        #   well_name='_896Б_'
+
+  
+
+        
         path=self.papka_with_models+'wellmodel_'+well_name+'.pickle'
+
+        
+        
         with open(path, "rb") as file:
           self.model = pickle.load(file)
     #data is 'Дебит жидкости', 'Газовый фактор', 'Буферное давление','Обводненность'
