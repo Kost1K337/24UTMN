df2faf8aba1fe6f972d1fe02599f3d938bd58ae8
a.e.kulishkin <zarelly2013@yandex.ru>
2024-04-19 16:08:30 +0500

    fix

--- src/tube/model.py
+++ src/tube/model.py
@@ -4,9 +4,12 @@ import zipfile
 class DTreeForIMA:
     def __init__(self, filename):
         self.filename = filename
+        self.unzip()
+        self.model = joblib.load(f'{self.filename}.pkl')
+
+    def unzip(self):
         with zipfile.ZipFile(f'{self.filename}.zip', 'r') as zip_ref:
             zip_ref.extractall()
-        self.model = joblib.load(f'tube/{self.filename}.pkl')
 
     def predict(self, X_test):
         pred_dtr = self.model.predict(X_test)
